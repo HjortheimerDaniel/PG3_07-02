866ac439173cb7153cd987ebfd9e48285cb6af83
HjortheimerDaniel <147121561+HjortheimerDaniel@users.noreply.github.com>
2024-06-07 16:20:26 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <thread>
+
+void Thread1(const char* text)
+{
+	printf("%s\n", text);
+}
+
+void Thread2(const char* text)
+{
+	printf("%s\n", text);
+}
+
+void Thread3(const char* text)
+{
+	printf("%s\n", text);
+}
+
+
+
+int main() {
+
+	const char* text1 = "thread1";
+	const char* text2 = "thread2";
+	const char* text3 = "thread3";
+
+	//put the texts inside seperate threads
+
+	std::thread thread1(Thread1, text1);
+	std::thread thread2(Thread2, text2);
+	std::thread thread3(Thread3, text3);
+
+	//wait for the all the threads to be finished then run
+	
+	thread1.join(); 
+	thread2.join();
+	thread3.join();
+	
+
+	return 0; }
